ffc174364b17742d95655d7854190e2d68cdb8cc
Tanay <tanayverma98@gmail.com>
2022-10-02 21:00:03 +0530

    patch completed

--- controllers/doctor.js
+++ controllers/doctor.js
@@ -2,36 +2,45 @@ const Doctor = require('../models/doctor')
 
 const getDoctors = async (req,res)=>{
     const {id:doctorID, name, city, phoneNumber, email, speciality, fields} = req.query
-
+    
     let queryObj = {}
+    // to get doctor by ID
     if(doctorID){
         queryObj._id = doctorID
     }
+    // to get doctor(s) by name
     if(name){
         queryObj.name = name
     }
+    // to get doctor(s) by city
     if(city){
         queryObj.city = city
     }
+    // to get doctor by phone number
     if(phoneNumber){
         queryObj.phoneNumber = phoneNumber
     }
+    // to get doctor by email ID
     if(email){
         queryObj.email = email
     }
+    // to get doctor(s) by their speciality
     if(speciality){
         queryObj.speciality = speciality
     }
 
     let doctor =  Doctor.find(queryObj)
-
+    // filtering to only get specified fields in the data
     if(fields){
         const fieldsString = fields.split(',').join(' ')
         doctor = doctor.select(fieldsString)
     }
-    if(doctor.length === 0) throw Error('no user found')
-
+    
     const data = await doctor
+
+    // if the no data is found
+    if(data.length === 0) throw Error('no user found')
+    // response to successful request
     res.json({
         success:true,
         data,
@@ -45,7 +54,17 @@ const createDoctor = async (req,res)=>{
 }
 
 const updateDoctor = async (req,res)=>{
-    
+    const {id:doctorID} = req.query
+    const doctor = await Doctor.findOneAndUpdate({_id:doctorID},req.body,{
+        new:true,
+        runValidators:true
+    })
+    if(!doctor) throw Error(`No doctor with id: ${doctorID}`)
+
+    res.status(200).json({
+        success:true,
+        data:doctor
+    })
 }
 
 const deleteAllDoctors = async(req,res)=>{
@@ -56,5 +75,6 @@ const deleteAllDoctors = async(req,res)=>{
 module.exports = {
     getDoctors,
     createDoctor,
+    updateDoctor,
     deleteAllDoctors
 }
--- routes/doctor.js
+++ routes/doctor.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
-const {getDoctors, createDoctor, deleteAllDoctors} = require('../controllers/doctor')
+const {getDoctors, createDoctor,updateDoctor ,deleteAllDoctors} = require('../controllers/doctor')
 
-router.route('/').get(getDoctors).post(createDoctor).patch().delete(deleteAllDoctors)
+router.route('/').get(getDoctors).post(createDoctor).patch(updateDoctor).delete(deleteAllDoctors)
 
 
 module.exports = router
